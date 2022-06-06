4b539964fccc1cd3a5778e5f0a5544694bbbef1d
JoseP-UR <urivesrosa@gmail.com>
2022-06-06 01:21:35 -0300

    some work on post listing component

--- front/components/PostItem.js
+++ front/components/PostItem.js
@@ -1,12 +1,17 @@
 import {
-    Box
+    Box, Container, Text, Heading, Image
 } from '@chakra-ui/react'
 
 export default function PostItem(props) {
     console.log('PostItem.js: props = ', props)
     return (
-        <Box>
-            hi
-        </Box>
+        <Container justifyContent={'space-around'} h="52" display={'flex'} p={'5'} boxShadow={'3px 4px 10px -5px black'} borderRadius="md" my="10">
+            <Image src={props.post.image} alt={props.post.title} boxSize="40" />
+            <Box flexBasis={'40%'}>
+                <Heading>{props.post.title}</Heading>
+                <Text>{props.post.date}</Text>
+                <Text noOfLines={['2', '3']}>{props.post.subtitle}</Text>
+            </Box>
+        </Container>
     )
 }
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,10 +1,15 @@
-import { Box } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 import PostItem from '../components/PostItem' 
-export default function Home() {
+export default function Home(props) {
+  console.log(props)
   return (
-    <Box>
-      <PostItem />
-    </Box>
+    <Grid height={'100vh'} templateColumns={['auto','auto 35%']}>
+      <GridItem>
+        {props.posts.map(post => (
+          <PostItem key={post.id} post={post} />
+        ))}
+      </GridItem>
+    </Grid>
   )
 }
 
@@ -12,24 +17,22 @@ export default function Home() {
 export function getStaticProps() {
   return {
     props: {
-      initialData: {
         posts: [
           {
             id: '1',
             title: 'Hello Next.js',
-            content: 'Learn Next.js by Example',
-            image: 'https://picsum.photos/200',
+            subtitle: 'Learn Next.js by Example',
             date: '2019-05-28T17:00:00.000Z',
+            image: 'https://picsum.photos/200',
           },
           {
             id: '2',
             title: 'Learn Next.js',
-            content: 'Learn Next.js by Example',
-            image: 'https://picsum.photos/200',
+            subtitle: 'Learn Next.js by ExampleLearn Next.js by ExampleLearn Next.js by ExampleLearn Next.js by ExampleLearn Next.js by ExampleLearn Next.js by ExampleLearn Next.js by ExampleLearn Next.js by ExampleLearn Next.js by Example',
             date: '2019-05-28T17:00:00.000Z',
+            image: 'https://picsum.photos/200',
           },
         ]
-      }
     }
   }
 }
